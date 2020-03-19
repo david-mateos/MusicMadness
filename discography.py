77054cc36da953b32b52561910eb747e7231bb64
David Mateos <dmateos6@gmail.com>
2020-03-19 18:41:10 -0400

    change the dict keys to ids
    need to account for different version of songs, dict keys should be ids and not track names

--- discography.py
+++ discography.py
@@ -43,16 +43,23 @@ def get_tracks(playlist_ids):
 def get_track_popularity(track_ids):
     pop = {}
     results = sp.tracks(track_ids, market='US')
-    for i, track in enumerate(results['tracks']):
-        pop[(track['name'])] = track['popularity']
+    for i, song in enumerate(results['tracks']):
+        pop[(song['id'])] = song['popularity']
     return pop
 
 def get_track_artist(track_ids):
     artist = {}
     results = sp.tracks(track_ids, market='US')
-    for i, track in enumerate(results['tracks']):
-        artist[(track['name'])] = track['artists'][0]['name']
+    for i, song in enumerate(results['tracks']):
+        artist[(song['name'])] = song['artists'][0]['name']
     return artist
+
+def get_track_name(track_ids):
+    track = {}
+    results = sp.tracks(track_ids, market='US')
+    for i, song in enumerate(results['tracks']):
+        track[(song['id'])] = song['name']
+    return track
 
 
 if __name__ == "__main__":
@@ -62,17 +69,19 @@ if __name__ == "__main__":
    
    if len(all_tracks) >= 32:
        
-       n = 50 # spotify only lets us do this 50 tracks at a time
+       n = 50
        chunk = [all_tracks[i:i + n] for i in range(0, len(all_tracks), n)]
        
        all_popularities = {}
        track_artist = {}
+       track = {}
        for element in list(chunk):
            all_popularities.update(get_track_popularity(element))
            track_artist.update(get_track_artist(element))
+           track.update(get_track_name(element))
        
        output_file = 'bracket_tracks.csv'
-       data = [all_popularities.keys(),all_popularities.values(),track_artist.values()]
+       data = [track.values(),all_popularities.values(),track_artist.values()]
        with open(output_file, 'w') as f:
            w = csv.writer(f)
            w.writerows(zip(*data))
